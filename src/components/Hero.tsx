207a2e4f04a87dfd5e0dea4846a88f3803ac2fae
Ahmed Sakr <ahmedaesakr@gmail.com>
2025-12-14 15:56:06 +0200

    Update hero description text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -107,8 +107,7 @@ export default function Hero() {
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.8, delay: 0.6 }}
                     >
-                        Crafting immersive CGI experiences <br />
-                        and dynamic DOOH campaigns.
+                        Anamorphic DOOH - CGI - WebGL (Three.js/R3F)
                     </motion.p>
 
                     <motion.div
